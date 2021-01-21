a16176ad688e105253895b9ded71a1528d814bac
Hongbusi <hongyongchun@edusoho.com>
2021-01-21 19:51:34 +0800

    add reset

--- bin/index.js
+++ bin/index.js
@@ -8,6 +8,7 @@ const upgrade = require('../lib/upgrade');
 const addTemplate = require('../lib/addTemplate');
 const removeTemplate = require('../lib/removeTemplate');
 const init = require('../lib/init');
+const resetTemplate = require('../lib/resetTemplate');
 
 const { packagePath } = require('../lib/filePath');
 
@@ -46,6 +47,14 @@ program
   .description('remove a template')
   .action((templateName) => {
     removeTemplate(templateName);
+  });
+
+// reset
+program
+  .command('reset')
+  .description('resetting the default template')
+  .action(() => {
+    resetTemplate();
   });
 
 // init
