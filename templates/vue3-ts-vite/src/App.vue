7bb8d8e041694f1cb4a645f3fcd8be446f60f3b8
Hongbusi <q951686662@qq.com>
2022-01-04 17:00:06 +0800

    feat: init vue3-ts-vite

---
+++ templates/vue3-ts-vite/src/App.vue
@@ -0,0 +1,10 @@
+
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
